a85b10cd9cb1642e77a5da613ece412fb0adcdeb
Charles Emmanuel <emmaxcharles123@gmail.com>
2023-10-12 15:34:43 +0100

    Building web servers and web service with rocket

---
+++ src/main.rs
@@ -0,0 +1,112 @@
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_migrations;
+
+
+
+mod auth;
+mod models;
+mod schema;
+mod repositories;
+
+
+use auth::BasicAuth;
+use models::{Rustacean, NewRustacean};
+
+use rocket::Build;
+use rocket::Rocket;
+use rocket::serde::json::{Value, json, Json};
+use rocket::response::status;
+use rocket::http::Status;
+use rocket::fairing::AdHoc;
+use rocket_sync_db_pools::database;
+use repositories::RustaceanRepository;
+use diesel_migrations::run_migrations;
+
+#[database("sqlite_logs")]
+struct DBConn(rocket_sync_db_pools::diesel::SqliteConnection);
+
+// embed_migrations!();
+
+#[get("/rustaceans")]
+async fn get_rustaceans(_auth: BasicAuth, db: DBConn) -> Result<Value, status::Custom<Value>>  {
+    db.run(|c| {
+        RustaceanRepository::find_mutiples(c, 100)
+            .map(|rustacean| json!(rustacean))
+            .map_err(|e| status::Custom(Status::InternalServerError, json!(e.to_string())))
+    }).await
+}
+
+#[get("/rustaceans/<id>")]
+async fn view_rustaceans(id: i32, _auth: BasicAuth, db: DBConn) -> Result<Value, status::Custom<Value>> {
+    db.run(move |c| {
+        RustaceanRepository::find(c, id)  
+            .map(|rustacean| json!(rustacean))
+            .map_err(|e| status::Custom(Status::InternalServerError, json!(e.to_string())))
+    }).await
+}
+
+#[post("/rustaceans", format = "json", data = "<new_rustacean>")]
+async fn create_rustaceans(_auth: BasicAuth, db: DBConn, new_rustacean: Json<NewRustacean>) -> Result<Value, status::Custom<Value>> {
+    db.run(|c| {
+        RustaceanRepository::create(c, new_rustacean.into_inner())
+            .map(|rustacean| json!(rustacean))
+            .map_err(|e| status::Custom(Status::InternalServerError, json!(e.to_string())))
+    }).await
+}
+
+#[put("/rustaceans/<id>", format = "json", data = "<rustacean>")]
+async fn update_rustaceans(id: i32, _auth: BasicAuth, db: DBConn, rustacean: Json<Rustacean>) -> Result<Value, status::Custom<Value>> {
+    db.run(move |c| {
+        RustaceanRepository::save(c, id, rustacean.into_inner())
+            .map(|rustacean| json!(rustacean))
+            .map_err(|e| status::Custom(Status::InternalServerError, json!(e.to_string())))
+    }).await
+}  
+
+#[delete("/rustaceans/<id>")]
+async fn delete_rustaceans(id: i32, _auth: BasicAuth, db: DBConn) -> Result<status::NoContent, status::Custom<Value>> {
+    db.run(move |c| {
+        RustaceanRepository::delete(c, id)
+            .map(|_| status::NoContent)
+            .map_err(|e| status::Custom(Status::InternalServerError, json!(e.to_string())))
+    }).await
+}
+
+#[catch(404)]
+fn not_found() -> Value {
+    json!("Not Found!")
+}
+
+async fn run_db_migrations(rocket: Rocket<Build>) -> Result<Rocket<Build>, Rocket<Build>> {
+    let conn = DBConn::get_one(&rocket).await
+        .expect("failed to retrieve database connection");
+
+    conn.run(|c| run_migrations(c, &MIGRATIONS)).await
+        .map(|_| rocket)
+        .map_err(|e| {
+            println!("Failed to run database migration: {:?}", e);
+            rocket
+        })
+}
+
+
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+        .mount("/", routes![
+            get_rustaceans,
+            view_rustaceans,
+            create_rustaceans,
+            update_rustaceans,
+            delete_rustaceans
+        ])
+        .register("/", catchers![
+            not_found
+        ])
+        .attach(DBConn::fairing())
+        .attach(AdHoc::try_on_ignite("Running DB migration", run_db_migrations))
+        .launch()
+        .await;
+}
